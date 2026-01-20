b67f3e670b28d3c7dea686af931c047efeb82e9d
yazioo <lfreefire940@gmail.com>
2026-01-20 04:44:57 -0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,8 +16,41 @@ const GithubIcon = memo(({ className = "" }: { className?: string }) => (
 
 GithubIcon.displayName = 'GithubIcon';
 
-// Define other icons (InstagramIcon, LinkedInIcon, BehanceIcon) similarly...
-// For brevity, I assume your previous definitions remain unchanged.
+const InstagramIcon = memo(({ className = "" }: { className?: string }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className={className}>
+        <defs>
+            <radialGradient id="skillIconsInstagram0" cx="0" cy="0" r="1" gradientTransform="matrix(0 -253.715 235.975 0 68 275.717)" gradientUnits="userSpaceOnUse"><stop stopColor="#FD5" /><stop offset=".1" stopColor="#FD5" /><stop offset=".5" stopColor="#FF543E" /><stop offset="1" stopColor="#C837AB" /></radialGradient>
+            <radialGradient id="skillIconsInstagram1" cx="0" cy="0" r="1" gradientTransform="matrix(22.25952 111.2061 -458.39518 91.75449 -42.881 18.441)" gradientUnits="userSpaceOnUse"><stop stopColor="#3771C8" /><stop offset=".128" stopColor="#3771C8" /><stop offset="1" stopColor="#60F" stopOpacity="0" /></radialGradient>
+        </defs>
+        <g fill="none">
+            <rect width="256" height="256" fill="url(#skillIconsInstagram0)" rx="60" />
+            <rect width="256" height="256" fill="url(#skillIconsInstagram1)" rx="60" />
+            <path fill="#ffffff" d="M128.009 28c-27.158 0-30.567.119-41.233.604c-10.646.488-17.913 2.173-24.271 4.646c-6.578 2.554-12.157 5.971-17.715 11.531c-5.563 5.559-8.98 11.138-11.542 17.713c-2.48 6.36-4.167 13.63-4.646 24.271c-.477 10.667-.602 14.077-.602 41.236s.12 30.557.604 41.223c.49 10.646 2.175 17.913 4.646 24.271c2.556 6.578 5.973 12.157 11.533 17.715c5.557 5.563 11.136 8.988 17.709 11.542c6.363 2.473 13.631 4.158 24.275 4.646c10.667.485 14.073.604 41.23.604c27.161 0 30.559-.119 41.225-.604c10.646-.488 17.921-2.173 24.284-4.646c6.575-2.554 12.146-5.979 17.702-11.542c5.563-5.558 8.979-11.137 11.542-17.712c2.458-6.361 4.146-13.63 4.646-24.272c.479-10.666.604-14.066.604-41.225s-.125-30.567-.604-41.234c-.5-10.646-2.188-17.912-4.646-24.27c-2.563-6.578-5.979-12.157-11.542-17.716c-5.562-5.562-11.125-8.979-17.708-11.53c-6.375-2.474-13.646-4.16-24.292-4.647c-10.667-.485-14.063-.604-41.23-.604h.031Zm-8.971 18.021c2.663-.004 5.634 0 8.971 0c26.701 0 29.865.096 40.409.575c9.75.446 15.042 2.075 18.567 3.444c4.667 1.812 7.994 3.979 11.492 7.48c3.5 3.5 5.666 6.833 7.483 11.5c1.369 3.52 3 8.812 3.444 18.562c.479 10.542.583 13.708.583 40.396c0 26.688-.104 29.855-.583 40.396c-.446 9.75-2.075 15.042-3.444 18.563c-1.812 4.667-3.983 7.99-7.483 11.488c-3.5 3.5-6.823 5.666-11.492 7.479c-3.521 1.375-8.817 3-18.567 3.446c-10.542.479-13.708.583-40.409.583c-26.702 0-29.867-.104-40.408-.583c-9.75-.45-15.042-2.079-18.57-3.448c-4.666-1.813-8-3.979-11.5-7.479s-5.666-6.825-7.483-11.494c-1.369-3.521-3-8.813-3.444-18.563c-.479-10.542-.575-13.708-.575-40.413c0-26.704.096-29.854.575-40.396c.446-9.75 2.075-15.042 3.444-18.567c1.813-4.667 3.983-8 7.484-11.5c3.5-3.5 6.833-5.667 11.5-7.483c3.525-1.375 8.819-3 18.569-3.448c9.225-.417 12.8-.542 31.437-.563v.025Zm62.351 16.604c-6.625 0-12 5.37-12 11.996c0 6.625 5.375 12 12 12s12-5.375 12-12s-5.375-12-12-12v.004Zm-53.38 14.021c-28.36 0-51.354 22.994-51.354 51.355c0 28.361 22.994 51.344 51.354 51.344c28.361 0 51.347-22.983 51.347-51.344c0-28.36-22.988-51.355-51.349-51.355h.002Zm0 18.021c18.409 0 33.334 14.923 33.334 33.334c0 18.409-14.925 33.334-33.334 33.334c-18.41 0-33.333-14.925-33.333-33.334c0-18.411 14.923-33.334 33.333-33.334Z" />
+        </g>
+    </svg>
+));
+
+InstagramIcon.displayName = 'InstagramIcon';
+
+const LinkedInIcon = memo(({ className = "" }: { className?: string }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className={className}>
+        <g fill="none">
+            <rect width="256" height="256" fill="#fff" rx="60" />
+            <rect width="256" height="256" fill="#0A66C2" rx="60" />
+            <path fill="#fff" d="M184.715 217.685h29.27a4 4 0 0 0 4-3.999l.015-61.842c0-32.323-6.965-57.168-44.738-57.168c-14.359-.534-27.9 6.868-35.207 19.228a.32.32 0 0 1-.595-.161V101.66a4 4 0 0 0-4-4h-27.777a4 4 0 0 0-4 4v112.02a4 4 0 0 0 4 4h29.268a4 4 0 0 0 4-4v-55.373c0-15.657 2.97-30.82 22.381-30.82c19.135 0 19.383 17.916 19.383 31.834v54.364a4 4 0 0 0 4 4ZM38 59.627c0 11.865 9.767 21.627 21.632 21.627c11.862-.001 21.623-9.769 21.623-21.631C81.253 47.761 71.491 38 59.628 38C47.762 38 38 47.763 38 59.627Zm6.959 158.058h29.307a4 4 0 0 0 4-4V101.66a4 4 0 0 0-4-4H44.959a4 4 0 0 0-4 4v112.025a4 4 0 0 0 4 4Z" />
+        </g>
+    </svg>
+));
+
+LinkedInIcon.displayName = 'LinkedInIcon';
+
+const BehanceIcon = memo(({ className = "" }: { className?: string }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26" className={className}>
+        <path fill="#3b38f5" d="M19.5 12.4c.2.3.4.6.5 1.1h-3.2c0-.1 0-.3.1-.5s.1-.3.3-.5c.1-.2.3-.3.5-.4c.2-.1.5-.2.8-.2c.4.1.8.2 1 .5zm-9.2-.7c.2-.2.4-.5.4-.9c0-.2 0-.4-.1-.6c-.1-.1-.2-.3-.3-.3c-.1-.1-.3-.1-.5-.2H7.2V12h2.2c.3 0 .6-.1.9-.3zm-.8 1.7H7.2v2.7h2.3c.2 0 .4 0 .6-.1c.2 0 .4-.1.5-.2c.1-.1.3-.2.4-.4c.1-.2.1-.4.1-.6c0-.5-.1-.9-.4-1.1c-.3-.2-.7-.3-1.2-.3zM26 4.9v16.2c0 2.7-2.2 4.9-4.9 4.9H4.9C2.2 26 0 23.8 0 21.1V4.9C0 2.2 2.2 0 4.9 0h16.2C23.8 0 26 2.2 26 4.9zm-9.6 4.7h4v-1h-4v1zM13.2 15c0-.6-.1-1.1-.4-1.6c-.3-.4-.7-.7-1.3-.9c.4-.2.8-.5 1-.8c.2-.3.3-.7.3-1.2s-.1-.8-.2-1.2c-.2-.3-.4-.6-.6-.7c-.3-.2-.6-.3-1-.4c-.5-.2-.9-.2-1.4-.2H5v9.8h4.8c.4 0 .9-.1 1.3-.2s.8-.3 1.1-.5c.3-.2.6-.5.8-.9c.1-.3.2-.7.2-1.2zm3.6-.3h5.1c0-.6 0-1.1-.1-1.6s-.3-1-.6-1.3c-.3-.4-.7-.7-1.1-.9c-.5-.2-1-.4-1.6-.4c-.5 0-1 .1-1.5.3c-.4.2-.8.5-1.2.8c-.3.3-.6.7-.7 1.2c-.2.5-.3 1-.3 1.5s.1 1.1.3 1.5c.2.5.4.9.7 1.2c.3.3.7.6 1.1.8c.5.2 1 .3 1.5.3c.8 0 1.5-.2 2.1-.6c.6-.4 1-1 1.3-1.8h-1.7c-.1.2-.2.4-.5.6c-.3.2-.6.3-1 .3c-.5 0-1-.1-1.3-.4c-.3-.4-.5-.9-.5-1.5z" />
+    </svg>
+));
+
+BehanceIcon.displayName = 'BehanceIcon';
 
 const menuItems = [
     { name: 'Home', icon: Home },
@@ -41,7 +74,13 @@ const LogoSVG = memo(() => (
         xmlns="http://www.w3.org/2000/svg"
         className="drop-shadow-sm"
     >
-        {/* Your Logo paths here */}
+        <g fill="none">
+            <path fill="#48eeff" stroke="#231f20" strokeMiterlimit="10" d="M38.75 20a33 33 0 0 0-1.54-10c0-.51-3.25-4.12-6.56-5.92S21.92.18 19.5.54c-1.66-.06-6.66 1.7-10.06 3.55S3.06 8.68 2.91 9.56A34.3 34.3 0 0 0 1.25 20a34.3 34.3 0 0 0 1.66 10.44c.15.87 3.09 3.56 6.53 5.47s8.4 3.61 10.06 3.55c2.42.36 7.73-1.69 11.15-3.55s6.6-5.41 6.56-5.92A33 33 0 0 0 38.75 20Z" strokeWidth="1" />
+            <path stroke="#231f20" strokeLinecap="round" strokeMiterlimit="10" d="M7.25 14.32c.71.54 1.46 1 2.19 1.51C12.85 18 18.13 20.11 20.59 20c1.65-.1 6.65-2 10.06-4.12c.72-.45 1.43-.95 2.1-1.46" strokeWidth="1" />
+            <path stroke="#231f20" strokeLinecap="round" strokeMiterlimit="10" d="M20 32.5V22.32c0-2.32 1.73-2.57 1.73-2.57" strokeWidth="1" />
+            <path stroke="#231f20" strokeLinecap="round" strokeMiterlimit="10" d="M20 22.32a2.59 2.59 0 0 0-1.71-2.64" strokeWidth="1" />
+            <path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" d="M26.13 5.34A17.6 17.6 0 0 1 30.77 8" strokeWidth="1" />
+        </g>
     </svg>
 ));
 
@@ -116,7 +155,6 @@ const Navbar: React.FC<NavbarProps> = memo(({ currentView, onNavigate }) => {
                         </button>
                     </div>
 
-                    {/* Social links */}
                     <div className="relative group/contact">
                         <AnimatePresence>
                             {isContactOpen && (
@@ -137,10 +175,7 @@ const Navbar: React.FC<NavbarProps> = memo(({ currentView, onNavigate }) => {
                                                 href={social.href}
                                                 target="_blank"
                                                 rel="noopener noreferrer"
-                                                onClick={(e) => {
-                                                    // Prevent default only for internal placeholders
-                                                    if (social.href.startsWith("#")) e.preventDefault();
-                                                }}
+                                        
                                                 custom={index}
                                                 className="absolute w-12 h-12 bg-black rounded-full flex items-center justify-center text-white shadow-xl border border-white/10 pointer-events-auto hover:bg-gray-900 hover:scale-110 transition-colors z-[-1]"
                                                 initial={{ x: 0, y: 0, opacity: 0, scale: 0.5 }}
@@ -199,7 +234,6 @@ const Navbar: React.FC<NavbarProps> = memo(({ currentView, onNavigate }) => {
                 </div>
             </nav>
 
-            {/* Mobile menu */}
             <AnimatePresence>
                 {isMobileMenuOpen && (
                     <motion.div
